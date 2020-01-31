a2b36f8eaed7786e2ad35a5974abb21a595abbda
wsio001 <willwcsio@gmail.com>
2020-01-31 14:07:44 -0500

    initial commit

---
+++ Desktop/mapper/src/map.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+
+function Map(){
+    return(
+        <GoogleMap 
+        defaultZoom = {10} 
+        defaultCenter = {{lat: 35.904613, lng: -79.046761}}
+        />
+    );
+
+}
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function Mapper(){
+    return(
+        <div style = {{ width: "100vw", height: "100vh"}} >
+            <WrappedMap 
+            googleMapURL = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBkCSVd8RRMzlp3oS_Vged0nPm0-8MFVHk'
+            loadingElement = {<div style = {{height: '100%'}} />}
+            containerElement = {<div style={{ height: '100%' }} />}
+            mapElement = {<div style={{ height: '100%' }} />}
+            />
+        </div>
+
+    );
+}
